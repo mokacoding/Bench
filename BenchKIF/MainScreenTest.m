7b5a54f30500a867eadfcd0bd6394d09e13c4382
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-03-27 19:19:58 +1100

    Add tests for "say hello" and "show elements"

--- BenchKIF/MainScreenTest.m
+++ BenchKIF/MainScreenTest.m
@@ -23,4 +23,37 @@
     [tester waitForViewWithAccessibilityLabel:@"mokacoding.com"];
 }
 
+/**
+ *  When I tap the "say hello" button, I see a greetings alert, so that I can be happier :)
+ */
+- (void)testSayHello {
+    [tester tapViewWithAccessibilityLabel:@"say hello"];
+    
+    [tester waitForViewWithAccessibilityLabel:@"Hello"];
+    [tester waitForViewWithAccessibilityLabel:@"Sup?"];
+    [tester waitForViewWithAccessibilityLabel:@"Dismiss"];
+    
+    [tester tapViewWithAccessibilityLabel:@"Dismiss"];
+    
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Hello"];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Sup?"];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Dismiss"];
+}
+
+/**
+ *  When I tap the "show elements" button, I see a listt of elements, so I can expand my knowledge
+ */
+- (void)testShowElements {
+    [tester tapViewWithAccessibilityLabel:@"show elements"];
+    
+    [tester waitForViewWithAccessibilityLabel:@"Elements"];
+    [tester waitForViewWithAccessibilityLabel:@"[H] Hydrogen (1)" ];
+    [tester waitForViewWithAccessibilityLabel:@"[Uuo] Ununoctium (118)"];
+    [tester waitForViewWithAccessibilityLabel:@"Back"];
+    
+    [tester tapViewWithAccessibilityLabel:@"Back"];
+    
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Elements"];
+}
+
 @end
