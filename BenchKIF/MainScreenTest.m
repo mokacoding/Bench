6176b7ccc498c55ff5fb447d1a9bceb2e0c9f35f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-03-28 15:17:38 +1100

    Refactor: extract string values in accpetance tests

--- BenchKIF/MainScreenTest.m
+++ BenchKIF/MainScreenTest.m
@@ -10,6 +10,20 @@
 #import <XCTest/XCTest.h>
 #import <KIF.h>
 
+static NSString *kTitle = @"Bench";
+static NSString *kSayHello = @"say hello";
+static NSString *kShowElements = @"show elements";
+static NSString *kFooter = @"mokacoding.com";
+
+static NSString *kHelloAlertTitle = @"Hello";
+static NSString *kHelloAlertMessage = @"Sup?";
+static NSString *kHelloAlertButton = @"Dismiss";
+
+static NSString *kElementsTitle = @"Elements";
+static NSString *kElementsBack = @"Back";
+static NSString *kElementsFirst = @"[H] Hydrogen (1)";
+static NSString *kElementsLast = @"[Uuo] Ununoctium (118)";
+
 @interface MainScreenTest : XCTestCase
 
 @end
@@ -17,43 +31,43 @@
 @implementation MainScreenTest
 
 - (void)testUI {
-    [tester waitForViewWithAccessibilityLabel:@"Bench"];
-    [tester waitForViewWithAccessibilityLabel:@"say hello"];
-    [tester waitForViewWithAccessibilityLabel:@"show elements"];
-    [tester waitForViewWithAccessibilityLabel:@"mokacoding.com"];
+    [tester waitForViewWithAccessibilityLabel:kTitle];
+    [tester waitForViewWithAccessibilityLabel:kSayHello];
+    [tester waitForViewWithAccessibilityLabel:kShowElements];
+    [tester waitForViewWithAccessibilityLabel:kFooter];
 }
 
 /**
  *  When I tap the "say hello" button, I see a greetings alert, so that I can be happier :)
  */
 - (void)testSayHello {
-    [tester tapViewWithAccessibilityLabel:@"say hello"];
+    [tester tapViewWithAccessibilityLabel:kSayHello];
     
-    [tester waitForViewWithAccessibilityLabel:@"Hello"];
-    [tester waitForViewWithAccessibilityLabel:@"Sup?"];
-    [tester waitForViewWithAccessibilityLabel:@"Dismiss"];
+    [tester waitForViewWithAccessibilityLabel:kHelloAlertTitle];
+    [tester waitForViewWithAccessibilityLabel:kHelloAlertMessage];
+    [tester waitForViewWithAccessibilityLabel:kHelloAlertButton];
     
-    [tester tapViewWithAccessibilityLabel:@"Dismiss"];
+    [tester tapViewWithAccessibilityLabel:kHelloAlertButton];
     
-    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Hello"];
-    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Sup?"];
-    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Dismiss"];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:kHelloAlertTitle];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:kHelloAlertMessage];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:kHelloAlertButton];
 }
 
 /**
  *  When I tap the "show elements" button, I see a listt of elements, so I can expand my knowledge
  */
 - (void)testShowElements {
-    [tester tapViewWithAccessibilityLabel:@"show elements"];
+    [tester tapViewWithAccessibilityLabel:kShowElements];
     
-    [tester waitForViewWithAccessibilityLabel:@"Elements"];
-    [tester waitForViewWithAccessibilityLabel:@"[H] Hydrogen (1)" ];
-    [tester waitForViewWithAccessibilityLabel:@"[Uuo] Ununoctium (118)"];
-    [tester waitForViewWithAccessibilityLabel:@"Back"];
+    [tester waitForViewWithAccessibilityLabel:kElementsTitle];
+    [tester waitForViewWithAccessibilityLabel:kElementsFirst];
+    [tester waitForViewWithAccessibilityLabel:kElementsLast];
+    [tester waitForViewWithAccessibilityLabel:kElementsBack];
     
-    [tester tapViewWithAccessibilityLabel:@"Back"];
+    [tester tapViewWithAccessibilityLabel:kElementsBack];
     
-    [tester waitForAbsenceOfViewWithAccessibilityLabel:@"Elements"];
+    [tester waitForAbsenceOfViewWithAccessibilityLabel:kElementsTitle];
 }
 
 @end
