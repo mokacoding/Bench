292ff34244c9af88388a7f146cc6607bd8b48e6c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-04-13 08:49:17 +1000

    Rewrite ElementFormatter test using specta syntax

--- BenchTests/ElementFormatterTests.m
+++ BenchTests/ElementFormatterTests.m
@@ -1,42 +1,47 @@
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import <Specta.h>
+#import <Expecta.h>
 #import "ElementFormatter.h"
 
-@interface ElementFormatterTests : XCTestCase
+SpecBegin(ElementFormatter)
 
-@property (nonatomic, strong) ElementFormatter *formatter;
+describe(@"ElementFormatter", ^{
+    __block ElementFormatter *formatter;
+    
+    beforeAll(^{
+        formatter = [[ElementFormatter alloc] init];
+    });
+    
+    context(@"when formatting an element dictionary", ^{
+        context(@"when the dictionary is nil", ^{
+            it(@"returns nil", ^{
+                expect([formatter formattedElementString:nil]).to.beNil();
+            });
+        });
+        
+        context(@"when the dictionary is missing the name key", ^{
+            it(@"returns nil", ^{
+                expect([formatter formattedElementString:@{ @"symbol": @"E", @"atomic_number": @"42" }]).to.beNil();
+            });
+        });
+        
+        context(@"when the dictionary is missing the symbol key", ^{
+            it(@"returns nil", ^{
+                expect([formatter formattedElementString:@{ @"name": @"element", @"atomic_number": @"42" }]).to.beNil();
+            });
+        });
+        
+        context(@"when the dictionary is missing the atomic_number key", ^{
+            it(@"returns the [symbol] name format", ^{
+                expect([formatter formattedElementString:@{ @"name": @"element", @"symbol": @"E" }]).to.equal(@"[E] element");
+            });
+        });
+        
+        context(@"when the dictionary contains all the keys", ^{
+            it(@"returns the expected string it the [symbol] name (atomic number) format", ^{
+                expect([formatter formattedElementString:@{ @"name": @"element", @"symbol": @"E", @"atomic_number": @"42" }]).to.equal(@"[E] element (42)");
+            });
+        });
+    });
+});
 
-@end
-
-@implementation ElementFormatterTests
-
-- (void)setUp {
-    [super setUp];
-    self.formatter = [[ElementFormatter alloc] init];
-}
-
-- (void)testNilArgument {
-    XCTAssertNil([self.formatter formattedElementString:nil], @"ElementFormatter did not return nil when given nil data");
-}
-
-- (void)testMissingName {
-    NSDictionary *data = @{ @"symbol": @"E", @"atomic_number": @"42" };
-    XCTAssertNil([self.formatter formattedElementString:data], @"ElementFormatter did not return nil when given element data missing the name");
-}
-
-- (void)testMissingSymbol {
-    NSDictionary *data = @{ @"name": @"element", @"atomic_number": @"42" };
-    XCTAssertNil([self.formatter formattedElementString:data], @"ElementFormatter did not return nil when given element data missing the symbol");
-}
-
-- (void)testMissingAtomicNumber {
-    NSDictionary *data = @{ @"name": @"element", @"symbol": @"E" };
-    XCTAssertEqualObjects([self.formatter formattedElementString:data], @"[E] element", @"ElementFormatter did not format element data missing the atomic number as expected");
-}
-
-- (void)testSuccess {
-    NSDictionary *data = @{ @"name": @"element", @"symbol": @"E", @"atomic_number": @"42" };
-    XCTAssertEqualObjects([self.formatter formattedElementString:data], @"[E] element (42)", @"ElementFormatter did not format valid element data as expected");
-}
-
-@end
+SpecEnd
