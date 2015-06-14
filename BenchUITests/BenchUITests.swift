36436865a5c5886f7adb9e68805987fb9827224a
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-06-15 08:50:20 +1000

    Generate tests using the record button
    The record button is at the bottom left of the bottom bar, near the breakpoints toggle.

--- BenchUITests/BenchUITests.swift
+++ BenchUITests/BenchUITests.swift
@@ -1,11 +1,3 @@
-//
-//  BenchUITests.swift
-//  BenchUITests
-//
-//  Created by Giovanni on 15/06/2015.
-//  Copyright © 2015 mokacoding. All rights reserved.
-//
-
 import Foundation
 import XCTest
 
@@ -13,23 +5,35 @@ class BenchUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    /**
+     *  When I tap the "say hello button", I see a greetings alert.
+     */
+    func testSayHello() {
+        let app = XCUIApplication()
+        app.buttons["say hello"].tap()
+        app.alerts["Hello"].collectionViews.buttons["Dismiss"].tap()
     }
-    
+
+    /**
+     *  When I tap the "show elements" button, I see a list of elements.
+     */
+    func testShowElements() {
+        let app = XCUIApplication()
+        app.buttons["show elements"].tap()
+        app.tables.staticTexts["[N] Nitrogen (7)"].swipeUp()
+        app.tables.staticTexts["[Ir] Iridium (77)"].swipeUp()
+        app.tables.staticTexts["[Tl] Thallium (81)"].swipeUp()
+        app.tables.staticTexts["[Uut] Ununtrium (113)"].swipeUp()
+
+    }
+
 }
